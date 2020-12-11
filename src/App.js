7f7c9658804beb8050a498bf43e9559df84d3943
Isti <isticsek@gmail.com>
2020-12-11 20:49:18 +0200

    Job Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Navbar from "./components/Navbar"
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Services from "./pages/Services"
+import ContactUs from "./pages/ContactUs"
+import SignUp from "./pages/SignUp"
+import SignIn from "./pages/SignIn"
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Hello!</h1>
+    <div>
+      <Router>
+        <Navbar />
+        <Switch>
+          <div className='App'>
+            <Route path='/' component={Home} exact />
+            <Route path='/about' component={About} />
+            <Route path='/services' component={Services} />
+            <Route path='/contact-us' component={ContactUs} />
+            <Route path='/sign-up' component={SignUp} />
+            <Route path='/sign-in' component={SignIn} />
+          </div>
+        </Switch>
+      </Router>
     </div>
   )
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>
+        Welcome to my website <b>Visitor</b>!
+      </p>
+    </div>
+  )
+}
+
+export default Home
